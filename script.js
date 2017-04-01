5c25ff20f5909961694e6a1ab28dd27bb1582654
Zattri <daninjakidy@gmail.com>
2017-04-01 21:19:01 +0100

    Refractored out words for tags, about to parse URL to Clarifi

--- script.js
+++ script.js
@@ -7,38 +7,38 @@ var filterN = true;
 var filterS = true;
 
 function replaceImage(captionList, image) {
-    // If captionList contains banned words, replace them with danger
+    // If captionList contains banned Tags, replace them with danger
     // Replace image with placeholder image
 }
 
-function checkWord(inputWord) {
+function checkTag(inputTag) {
     if (filterG) {
             for (i = 0; i < bannedGore.length; i++) {
-                if (inputWord.toLowerCase() == bannedGore[i]) {
-                    inputWord = "Caution, this image may contain gore";
+                if (inputTag.toLowerCase() == bannedGore[i]) {
+                    inputTag = "Caution, this image may contain gore";
                 }
             }
     }
 
     if (filterN) {
             for (i = 0; i < bannedExplicit.length; i++) {
-                if (inputWord.toLowerCase() == bannedExplicit[i]) {
-                    inputWord = "Caution, this image may contain explicit images";
+                if (inputTag.toLowerCase() == bannedExplicit[i]) {
+                    inputTag = "Caution, this image may contain explicit images";
                 }
             }
     }
 
     if (filterS) {
             for (i = 0; i < bannedStuff.length; i++) {
-                if (inputWord.toLowerCase() == bannedStuff[i]) {
-                    inputWord = "Caution, this image may contain stuff";
+                if (inputTag.toLowerCase() == bannedStuff[i]) {
+                    inputTag = "Caution, this image may contain stuff";
                 }
             }
     }
 
-    // Add to a list of words contained in the image, and return the word list - DO LATER
-    console.log(inputWord); // Used for testing - Remove later
-    return inputWord;
+    // Add to a list of Tags contained in the image, and return the Tag list - DO LATER
+    console.log(inputTag); // Used for testing - Remove later
+    return inputTag;
 }
 
 // 1 for Gore, 2 for Explicit, 3 for Stuff [CURRENTLY UNUSED]
@@ -57,10 +57,10 @@ function setFilters(filterNum) {
 
 }
 
-function addBanned(inputWord, bannedList) {
+function addBanned(inputTag, bannedList) {
     for (i = 0; i < bannedList.length; i++) {
-        if (inputWord == bannedList[i]) {
-            bannedList.push(inputWord);
+        if (inputTag == bannedList[i]) {
+            bannedList.push(inputTag);
         }
     }
 }
@@ -71,25 +71,29 @@ function getBanned(bannedList) {
 
 
 function saveBanned() {
-    // Saves the banned words list to the JSON file again
+    // Saves the banned Tags list to the JSON file again
+}
+
+function parseImage(url) {
+
 }
 
 function main() {
-    // Loads the banned words list into a JS array to be used in JS - Only works on hosted server
-    console.log("[BANNED WORDS] Loading list");
+    // Loads the banned Tags list into a JS array to be used in JS - Only works on hosted server
+    console.log("[BANNED TAGS] Loading list");
     var JSONItems = [];
-    $.getJSON("bannedWords.json", function(data){
+    $.getJSON("bannedTags.json", function(data){
         JSONItems = data;
-        bannedWords = $.map(JSONItems, function(el) { return el; });
+        bannedTags = $.map(JSONItems, function(el) { return el; });
     });
-    console.log("[BANNED WORDS] Loading complete");
+    console.log("[BANNED TAGS] Loading complete");
 
-    // Testing the functions with certain words
+    // Testing the functions with certain Tags
     console.log(bannedExplicit);
     console.log(bannedGore);
-    checkWord("sausage");
-    checkWord("sword");
-    checkWord("thingy - unbanned");
+    checkTag("sausage");
+    checkTag("sword");
+    checkTag("thingy - unbanned");
 }
 
 main();
